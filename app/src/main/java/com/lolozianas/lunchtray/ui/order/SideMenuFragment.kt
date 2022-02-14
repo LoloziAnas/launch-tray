89fe5d497928765f9492e3879b1aaae1d55960dd
LoloziAnas <lolozianas1@gmail.com>
2022-02-14 20:30:01 +0000

    Implementing Side Menu Feature

--- app/src/main/java/com/lolozianas/lunchtray/ui/order/SideMenuFragment.kt
+++ app/src/main/java/com/lolozianas/lunchtray/ui/order/SideMenuFragment.kt
@@ -1,11 +1,15 @@
 package com.lolozianas.lunchtray.ui.order
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.lolozianas.lunchtray.R
+import com.lolozianas.lunchtray.databinding.FragmentSideMenuBinding
+import com.lolozianas.lunchtray.model.OrderViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -13,14 +17,52 @@ import com.lolozianas.lunchtray.R
  */
 class SideMenuFragment : Fragment() {
 
+    // Binding object instance corresponding to fragment_side_menu.xml layout
+    private var _binding: FragmentSideMenuBinding? = null
+    private val binding get() = _binding!!
+    // View Model instance
+    private val orderViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_side_menu, container, false)
+        _binding = FragmentSideMenuBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+            // Specify the lifecycle Owner of the view model as the lifecycle of the fragment
+            lifecycleOwner = viewLifecycleOwner
+            // Assign the view model
+            viewModel = orderViewModel
+            // Assign the fragment
+            sideMenuFragment = this@SideMenuFragment
 
+        }
+    }
+
+    /**
+     * Navigate to the [StartOrderFragment] and reset order
+     * */
+    fun cancelOrder() {
+        // Reset the order
+        orderViewModel.resetOrder()
+        // Navigate to the start fragment
+        findNavController().navigate(R.id.action_sideMenuFragment_to_startOrderFragment)
+    }
+
+    /**
+     * Navigate to the next screen [AccompanimentMenuFragment] to accomplish the order.
+     * */
+    fun goNextScreen() {
+        findNavController().navigate(R.id.action_sideMenuFragment_to_accompanimentMenuFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
